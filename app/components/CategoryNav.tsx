473753c06a12298668114e4e1689cf6b61e74db4
abr-ya <gamer05@list.ru>
2024-02-12 14:31:03 +0300

    feat: Add Hero component

---
+++ app/components/CategoryNav.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+const CategoryNav = () => {
+  const linkRender = (path: string) => (
+    <Link
+      href={`/${path.toLowerCase()}`}
+      className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
+    >
+      {path}
+    </Link>
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
+      <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
+        {linkRender("Men")}
+        {linkRender("Women")}
+        {linkRender("Teens")}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryNav;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import { Badge } from "@/components/ui/badge";
+import Hero from "./components/Hero";
 
 const Home = () => (
   <main className="flex min-h-screen flex-col items-center p-24">
-    <h1 className="text-xl mb-4">Hello, Next 14 + Tailwind CSS!</h1>
-    <Badge variant="default">Badge</Badge>
+    <Hero />
   </main>
 );
 
